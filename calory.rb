27752b65cace4131106755a7af6bbf5ece79755c
dan <ec2-user@ip-172-31-26-123.us-east-2.compute.internal>
2019-05-26 00:25:47 +0000

    配列とハッシュを利用

--- calory.rb
+++ calory.rb
@@ -1,50 +1,38 @@
 class Food
   
+  # 空のハッシュを用意
+  @menu_calory = {}
+  
   def Food.input
     puts "料理名を入力してください："
-    name = gets.chomp.to_s
-    File.open('menu.txt',"a") do |text|
-      text.puts("#{name}")
-    end
-    
+    @name = gets.chomp.to_s
     puts "カロリーを入力してください"
-    calory = gets.chomp.to_i
-    File.open('calory.txt',"a") do |text|
-      text.puts("#{calory}")
-    end
+    @calory = gets.chomp.to_i
+    # 用意したハッシュにキー：@nameと値：@caloryを追加する
+    @menu_calory[@name] = @calory
   end
   
   def Food.show_all_calory
     puts "-----------------------"
-    File.open('menu.txt') do |file|
-      file.each_line do |name|
-        puts name
-      end
+    # each文でハッシュ内のキーと値を羅列表示
+    @menu_calory.each do |menu,calory|
+      puts "#{menu} :#{calory}kcal"
     end
     puts "-----------------------"
-      
-    cal = []
     
-    file = File.open('calory.txt')
-      file.each_line do |name|
-        cal << name.to_i
-      end
-    file.close
-    
+    # 新たに空の配列を用意
+    @a = []
+    #空の配列にハッシュの値だけを取り入れる
+    #ブロック引数を１つにするとキーと値は配列に格納される性質を利用
+    @menu_calory.each do |calory|
+      @a << calory[1]
+    end
+
     sum = 0
-    cal.each do |n|
-      sum += n
+    @a.each do |b|
+      sum += b
     end
-    
-    puts "カロリー合計　　：#{sum}kcal"
-    
-    File.open('menu.txt', "w") do |file| 
-      file.print("")
-    end
-    
-    File.open('calory.txt', "w") do |file| 
-      file.print("")
-    end
+    puts  "合計#{sum}kcal"
     
   end
   
